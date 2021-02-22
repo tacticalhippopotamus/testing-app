6c922e1a5d33ab2bf02b40946ee2a6068c4ccb31
griggle <grigglethewise@gmail.com>
2021-02-22 14:19:34 +0000

    screens

---
+++ core/src/com/testing/screens/MainMenuScreen.java
@@ -0,0 +1,5 @@
+package com.testing.screens;
+
+public class MainMenuScreen extends MenuScreen {
+
+}
---
+++ core/src/com/testing/screens/PauseScreen.java
@@ -0,0 +1,5 @@
+package com.testing.screens;
+
+public class PauseScreen extends MenuScreen {
+
+}
